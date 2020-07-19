2f6591d15664d8cb03714db4f6df981979888a10
Fangming Qu <mingcomputerscience@gmail.com>
2020-07-19 04:20:26 -0400

    Recipe Edit Value

---
+++ src/components/RecipeEdit.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import RecipeIngredientEdit from './RecipeIngredientEdit';
+
+export default function RecipeEdit({ recipe }) {
+  return (
+    <div className="recipe-edit">
+      <div className="recipe-edit__remove-button-container">
+        <button className="btn recipe-edit__remove-button">&times;</button>
+      </div>
+      <div className="recipe-edit__details-grid">
+        <label htmlFor="" className="recipe-edit__label">
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={recipe.name}
+          className="recipe-edit__input"
+        />
+        <label htmlFor="" className="recipe-edit__label">
+          Cook Time
+        </label>
+        <input
+          type="text"
+          name="cookTime"
+          id="cookTime"
+          value={recipe.cookTime}
+          className="recipe-edit__input"
+        />
+        <label htmlFor="" className="recipe-edit__label">
+          Servings
+        </label>
+        <input
+          type="number"
+          min="1"
+          id="servings"
+          value={recipe.servings}
+          className="recipe-edit__input"
+        />
+        <label htmlFor="instructions" className="recipe-edit__label">
+          Instructions
+        </label>
+        <textarea name="instructions" id="instructions" className="recipe-edit__input">
+          {recipe.instructions}
+        </textarea>
+      </div>
+      <br />
+      <label className="recipe-edit__input">Ingredients</label>
+      <div className="recipe-edit__ingredient-grid">
+        <div>Name</div>
+        <div>Amount</div>
+        <div></div>
+        {recipe.ingredients.map((ingredient) => (
+          <RecipeIngredientEdit key={ingredient.id} ingredient={ingredient} />
+        ))}
+        <RecipeIngredientEdit />
+        <RecipeIngredientEdit />
+        <div className="recipe-edit__add-ingredient-btn-container">
+          <button className="btn btn--primary">Add Ingredient</button>
+        </div>
+      </div>
+    </div>
+  );
+}
